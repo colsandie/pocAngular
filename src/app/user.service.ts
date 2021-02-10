80bcc25559fb023aca19465070c975a4c3c9aa98
Diego Colchado <diego.colchado@neoris.com>
2021-02-09 18:06:06 -0600

    Ajuste de url para servicio back

--- src/app/user.service.ts
+++ src/app/user.service.ts
@@ -11,7 +11,7 @@ export class UserService {
   private usersUrl: string;
 
   constructor(private http: HttpClient) {
-    this.usersUrl = 'http://localhost:8080/users';
+    this.usersUrl = 'http://poc-java-git:8080/users';
    }
 
    public findAll(): Observable<User[]> {
